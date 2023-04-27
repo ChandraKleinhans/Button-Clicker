07bbb48aba9c5fa8c1a6c652c9f3c4c770f4b989
Chandra <chandra.kleinhans@gmail.com>
2023-04-27 14:35:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var btn = document.getElementById('define');
+
+function change()
+{
+    document.getElementById("login").value="Logout"; 
+}
+
+function showAlert()
+{
+    alert("Ninja was liked")
+}
+
+function defined()
+{
+document.getElementById("define").style.display = "none"
+}
